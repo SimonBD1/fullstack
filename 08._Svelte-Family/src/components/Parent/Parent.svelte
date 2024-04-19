fa4ce55178c451237ca017d7d82010859f6c3a93
SimonBD1 <simo20k3@stud.kea.dk>
2024-04-19 08:37:30 +0200

    started på 10

---
+++ 08._Svelte-Family/src/components/Parent/Parent.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let parentName;
+  export let myChildren;
+
+  import Child from "../Child/Child.svelte";
+
+  function handleShowLove(childName) {
+      console.log(`${parentName} is loved by ${childName} veeeeery much.`);
+  }
+
+  let treasureChest = ["🔱", "🏅", "⭐️", "🔑", "🏆", "👑", "🌟"];
+
+  function handleTakeFromTreasureChest(childName) {
+      treasureChest.pop();
+      treasureChest = treasureChest;
+  }
+
+  // assignment give parents the power to wipe the fridge
+</script>
+
+<h1>{parentName}</h1>
+
+<p>
+{#each treasureChest as treasure}
+  {treasure + " "}
+{/each}
+</p>
+
+{#each myChildren as child}
+  <Child child={child} onShowLove={handleShowLove} 
+      onTakeFromTreasureChest={handleTakeFromTreasureChest} />
+{/each}
